3774eaf74c83a2b7935563047477ee435f7568d3
贺永胜 <1378431028@qq.com>
2023-04-13 22:00:11 +0800

    增加src别名配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@/src/assets/style/normalize.css'
+import '@/assets/style/normalize.css'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +16,9 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
